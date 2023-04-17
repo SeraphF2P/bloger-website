ec5b4764bd7f0dfca87c4548279a0f215f2e129d
SeraphF2P <110803319+SeraphF2P@users.noreply.github.com>
2023-04-17 17:46:22 +0300

    upload

--- src/env.mjs
+++ src/env.mjs
@@ -6,9 +6,7 @@ import { z } from "zod";
  */
 const server = z.object({
   DATABASE_URL: z.string().url(),
-  NODE_ENV: z.enum(["development", "test", "production"]),
-  GOOGLE_CLIENT_ID: z.string().min(1)
-  , GOOGLE_CLIENT_SECRET: z.string().min(1)
+  NODE_ENV: z.enum(["development", "test", "production"])
   , NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: z.string().min(1)
   , CLERK_SECRET_KEY: z.string().min(1)
 
@@ -31,8 +29,6 @@ const client = z.object({
 const processEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
-  GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
   CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
