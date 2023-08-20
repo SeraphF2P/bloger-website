b5e9c23ea84265a56a799d5ae88871bb656abc55
SeraphF2P <110803319+SeraphF2P@users.noreply.github.com>
2023-08-20 18:49:05 +0300

    remake

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,44 +1,30 @@
+import { Icons } from "@/components";
 import Link, { type LinkProps } from "next/link";
 import type { ReactNode } from "react";
-import { Icons } from "@/components";
-import { cn, btnVariants } from "@/lib/cva";
-import { useUser } from "@clerk/nextjs";
 
 interface ListLink {
   children: ReactNode;
   href: LinkProps["href"];
-  disabled?: boolean;
 }
-const ListLink = ({ children, href, disabled = false }: ListLink) => {
+const ListLink = ({ children, href }: ListLink) => {
   return (
-    <li
-      className={cn(
-        btnVariants({
-          variant: "outline",
-          disable: disabled ? "link" : "default",
-        }),
-        "h-10 w-10 "
-      )}
-    >
-      <Link className={disabled ? " cursor-auto" : ""} href={href}>
-        {children}
-      </Link>
+    <li className={"h-10 w-10 "}>
+      <Link href={href}>{children}</Link>
     </li>
   );
 };
 
 const Navbar = () => {
-  const { isSignedIn } = useUser();
   return (
     <header className=" fixed left-0 top-0 z-40 flex w-full justify-center   ">
       <ul className="flex w-full max-w-screen-xsm items-center justify-between bg-white p-6 text-primary  shadow dark:bg-black">
         <ListLink href="/">
           <Icons.news />
         </ListLink>
-        <ListLink disabled={!isSignedIn} href="/drafts">
+        <ListLink href="/drafts">
           <Icons.drafts />
         </ListLink>
-        <ListLink disabled={!isSignedIn} href="/profile">
+        <ListLink href="/profile">
           <Icons.userIcon />
         </ListLink>
         <ListLink href="/search">
