07557eb3453d051b698ad3d08dea729cabedb34f
SeraphF2P <110803319+SeraphF2P@users.noreply.github.com>
2023-08-30 21:20:01 +0300

    changed the scrollbar poisiton

--- src/components/ui/Container.tsx
+++ src/components/ui/Container.tsx
@@ -30,8 +30,8 @@ const Container = (props: { children?: ReactNode; className?: string }) => {
       {props.children}
 
       <ScrollBar container={ref}>
-        <Track className="z-40 w-2 transition-[width] duration-300 group hover:w-6 flex left-0 bottom-0  top-0 bg-revert-theme/20 ">
-          <Thumb className=" w-1 transition-[width] duration-300 group-hover:w-2 bg-primary" />
+        <Track className="z-40 rotate-90 origin-bottom-left  w-4 h-[100vw] flex left-0  bottom-8    bg-revert-theme/20 ">
+          <Thumb className=" w-2 bg-primary" />
         </Track>
       </ScrollBar>
     </m.main>
